8a6b3358be746361a7f8286939961772655a5486
lguillain <leonore.guillain@bluewin.ch>
2016-11-21 18:41:32 +0800

    Update event.js

--- app/js/event.js
+++ app/js/event.js
@@ -1,6 +1,22 @@
 var app = angular.module('teamformApp');
-app.controller('displayEventCtrl', ['$scope', '$firebaseArray',
-  function($scope, $firebaseArray){
+app.controller('displayEventCtrl', ['$scope', '$firebaseArray', 'searchService',
+  function($scope, $firebaseArray, searchService){
+      $scope.searchText = "";
+      $scope.startSearch = function(){
+        $scope.events = searchService.startSearchEvent($scope.searchText)
+      };
       var ref= firebase.database().ref('TeamForm/events');
       $scope.events = $firebaseArray(ref);
-}]);
+}])
+.controller('eventCtrl', ['$scope', '$firebaseObject', '$stateParams', '$firebaseArray',
+ function($scope, $firebaseObject, $stateParams, $firebaseArray){
+ var ref= firebase.database().ref('TeamForm/events/'+ $stateParams.id);
+ var refTeams = firebase.database().ref('TeamForm/teams/').orderByChild("event").equalTo($stateParams.id);
+ $scope.event = $firebaseObject(ref);
+ //$scope.teams = $firebaseArray(refTeams);
+ $scope.teams = $firebaseArray(refTeams);
+ console.log($scope.teams);
+  //console.log($scope.teams);
+
+
+}])
